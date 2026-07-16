aacbbf6e7c75d37729cbfe694d8cb7717eee8ddf
patelvrushti03 <patelvrushti03@gmail.com>
2026-07-16 18:54:22 +0530

    Add new feature

---
+++ students/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    roll_no = models.IntegerField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    course = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/',null=True)
+
+    def __str__(self):
+        return self.name
