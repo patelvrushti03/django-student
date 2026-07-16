edda663e4d41d258056cba16581cc4f6073c2107
patelvrushti03 <patelvrushti03@gmail.com>
2026-07-16 18:48:26 +0530

    Remove all files except README

--- students/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'roll_no', 'email', 'course']
-
-admin.site.register(Student, StudentAdmin)
